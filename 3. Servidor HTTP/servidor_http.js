9a5422f23cb39e9b64fd2f48196f3c76a922cd27
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-09-01 23:55:19 +0000

    Extensiones al servidor para poder servir imágenes PNG.

--- 3. Servidor HTTP/servidor_http.js
+++ 3. Servidor HTTP/servidor_http.js
@@ -3,26 +3,53 @@
 'use strict';
 
 const net = require('net');
+const fs = require('fs');
 const PUERTO = process.env['PORT'] || '8080';
 
-const MENSAJE = new Buffer('¡Este niño está muy ñoño!');
+const MENSAJE = new Buffer(
+`<h1>404 Not Found</h1>
+<p>
+  El recurso solicitado no pudo ser encontrado. ¡Menso!
+</p>
+`);
 
 const server = net.createServer((socket) => {
 
-  console.log('client connected');
-
   socket.on('data', (data) => {
-    socket.write(
+    let entrada = data.toString();
+    let lineas = entrada.split('\r\n');
+    let lineaPeticion = lineas[0];
+    let peticion = lineaPeticion.split(' ');
+    let metodo = peticion[0];
+    let recurso = peticion[1];
+    let protocolo = peticion[2];
+    console.log(lineaPeticion);
+    
+    fs.readFile(recurso.substring(1), (err, data) => {
+      if (err) {
+        socket.write(
+`HTTP/1.1 404 Not Found
+Server: MicroServerNode/0.1
+Date: ${ new Date().toUTCString() }
+Content-Type: text/html; charset=UTF-8
+Content-Length: ${ MENSAJE.length }
+
+`);
+        socket.write(MENSAJE);
+        socket.end();
+      } else {
+        socket.write(
 `HTTP/1.1 200 Ok
 Server: MicroServerNode/0.1
 Date: ${ new Date().toUTCString() }
-Content-Type: text/plain; charset=utf-8
-Content-Length: ${ MENSAJE.length }
+Content-Type: image/png
+Content-Length: ${ data.length }
 
-`
-    );
-    socket.write(MENSAJE);
-    socket.end();
+`);
+        socket.write(data);
+        socket.end();
+      }
+    });
   });
 });
 
@@ -31,5 +58,6 @@ server.on('error', (err) => {
 });
 
 server.listen(PUERTO, () => {
+  console.log(process.env['C9_HOSTNAME']);
   console.log('HTTP server bound on port ' + PUERTO);
 });
