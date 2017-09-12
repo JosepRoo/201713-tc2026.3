2f6aa959bf7f28bf75c96e861899bc9357c615a1
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-09-12 21:39:20 +0000

    Solución al primer examen práctico.

--- examen1/problema1.js
+++ examen1/problema1.js
@@ -1,5 +1,40 @@
 //---------------------------------------------------------
-// Coloca aquí tu matrícula y nombre.
+// Solución al problema 1.
 //---------------------------------------------------------
 
 'use strict';
+
+const net = require('net');
+const PUERTO = process.env['PORT'] || 8080;
+
+const server = net.createServer((socket) => {
+
+  console.log('Cliente conectado');
+
+  socket.on('end', () => {
+    console.log('Cliente desconectado');
+  });
+
+  let acumulado = 0;
+
+  socket.on('data', (data) => {
+    let entrada = data.toString().trim();
+    if (entrada === 'fin') {
+      socket.end(`Total: ${ acumulado }\n`);
+    } else if (/^\d+$/.test(entrada)) {
+      acumulado += parseInt(entrada, 10);
+      socket.write(`Acumulado: ${ acumulado }\n`);
+    } else {
+      socket.write('Valor no reconocido\n');
+    }
+  });
+
+});
+
+server.on('error', (err) => {
+  throw err;
+});
+
+server.listen(PUERTO, () => {
+  console.log('Servidor escuchando en puerto ' + PUERTO);
+});
--- examen1/problema2.js
+++ examen1/problema2.js
@@ -1,6 +1,35 @@
 //---------------------------------------------------------
-// Coloca aquí tu matrícula y nombre.
+// Solución al problema 2.
 //---------------------------------------------------------
 
 'use strict';
 
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+let parser = new xml2js.Parser();
+
+let nombreArchivo = process.argv[2];
+
+fs.readFile(nombreArchivo, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    parser.parseString(data, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (result['lista-de-numeros']['numero']) {
+          let lista = result['lista-de-numeros']['numero'];
+          let suma = 0;
+          for (const num of lista) {
+            suma += parseInt(num, 10);
+          }
+          console.log(suma / lista.length);
+        } else {
+          console.log('La lista de números está vacía');
+        }
+      }
+    });
+  }
+});
