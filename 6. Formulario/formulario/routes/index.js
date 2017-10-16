7c2ed68a0ce61c79654ce91f90f36f52118c9b9e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2017-10-16 23:04:04 +0000

    Conclusión del ejemplo del formulario.

--- 6. Formulario/formulario/routes/index.js
+++ 6. Formulario/formulario/routes/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
@@ -12,4 +12,18 @@ module.exports = router;
 
 router.get('/formulario', (req, res) => {
   res.render('formulario', {});
+});
+
+router.post('/recepcion', (req, res) => {
+  // req.query es para el método get
+  // req.body es para el método post
+  req.session.nombre = req.body.nombre;
+  req.session.apellido = req.body.apellido;
+  res.redirect('/final');
+});
+
+router.get('/final', (req, res) => {
+  let nombre = req.session['nombre'] || 'Juan';
+  let apellido = req.session['apellido'] || 'Cameney';
+  res.render('gracias', {nombre, apellido});
 });
